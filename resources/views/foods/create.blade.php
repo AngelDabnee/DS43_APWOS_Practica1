da1bc80964d53390b4c4c3fcf3fb99e023b1beb4
AngelDabnee <a22311046@uthermosillo.edu.mx>
2023-12-05 17:50:35 -0700

    modificamos el create para que funcionara
    Signed-off-by: AngelDabnee <a22311046@uthermosillo.edu.mx>

--- resources/views/foods/create.blade.php
+++ resources/views/foods/create.blade.php
@@ -2,8 +2,8 @@
 @section('title','create')
 @section('body')
 <div class="container">
-    @if ($foods)
-        <h1 class="text-center mb-0">Actualizar al Alimento: {{$foods->name}}</h1>
+    @if (isset($foods))
+        <h1 class="text-center mb-0">Actualizar al Alimento: {{old('name',isset($foods->name))}}</h1>
     @else
         <h1 class="text-center mb-0">Añadir al Alimento</h1>
     @endif
@@ -25,10 +25,10 @@
                 </div>
                
             @endif
-            @if ($foods)
+            @if (isset($foods))
                 <form method="POST" action="{{route ('foods.update') }}">
                     @method('PATCH')
-                    <input type="hidden" name="id" value="{{$foods->id}}">
+                    <input type="hidden" name="id" value="{{old('name',isset($foods->id))}}">
 
             @else
                 <form method="POST" action="{{route ('foods.store') }}">
@@ -37,29 +37,32 @@
                 @csrf  
                 <div id = "cardFoods" class="card text-center">
                     <label>Nombre</label>
-                    <input type="text" name = "name" class="form-control" placeholder="Nombre del Alimento" value = "{{$foods->name}}" required>
+                    <input type="text" name = "name" class="form-control" placeholder="Nombre del Alimento" value = "{{old('name',isset($foods->name))}}" required>
                     @if ($errors->has('name'))
                         <p class="text-danger">{{$errors->first('name')}}</p>                        
                     @endif
                     <label>URL de la Imagen</label>
-                    <input type="text" name="img" class="form-control" placeholder="Imagen del Alimento" value="{{$foods->img}}">
+                    <input type="text" name="img" class="form-control" placeholder="Imagen del Alimento" value="{{old('img',isset($foods->img))}}">
                     @if ($errors->has('img'))
                         <p class="text-danger">{{$errors->first('img')}}</p>                        
                     @endif
                     <label>Descripción</label>
-                    <input type="text" name = "description" class="form-control" placeholder="Descripción" value="{{$foods->description}}" required>
+                    <input type="text" name = "description" class="form-control" placeholder="Descripción" value="{{old('description',isset($foods->description))}}" required>
                     @if ($errors->has('description'))
                         <p class="text-danger">{{$errors->first('description')}}</p>                        
                     @endif
                     <label>Composición Nutricional</label>
                     <select name="composition_id" id="" class = "form-control" required>
-
-                        @if (!$foods)
+                        
+                        @if (!isset($foods))
                             <option value="">Selecciona la Composición</option>
-                        @else
-                        <option value="{{$foods->composition->id}}">{{$foods->composition->name}}</option>
                             @foreach ($compositions as $composition)
-                                @if ($foods->composition != $composition)
+                                <option value="{{$composition->id}}">{{$composition->name}}</option>
+                        @endforeach
+                        @else
+                            @foreach ($compositions as $composition)
+                            <option value="{{$composition->id}}">{{$composition->name}}</option>
+                                @if ($composition != $composition)
                                     <option value="{{$composition->id}}">{{$composition->name}}</option>
                                 @endif
                             @endforeach
@@ -74,7 +77,7 @@
         </div>
         <div class="col-6 mb-3">
             <div class="card">
-                @if ($foods->img)
+                @if (old('img',isset($foods->img)))
                     <img id = "imgFoodUpdate" src="{{$foods->img}}" class="img-fluid d-block w-100" >
                 @else
                     <img src="https://canalsalud.imq.es/hubfs/imq-blog/alimentos-saludables.jpg" class="d-block w-100" alt="Comida 1">
